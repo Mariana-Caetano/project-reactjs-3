4fb4c8e7933e2550bacfa6b08084a96972dc5010
Mariana Caetano Pereira <nanah.caetano@gmail.com>
2022-07-10 16:53:20 -0300

    first commit

---
+++ src/pages/Portfolio/Portfolio.jsx
@@ -0,0 +1,14 @@
+import Header from "../../components/Header/Header"
+import image from '../../assets/undraw_mobile_analytics.svg'
+
+function Portfolio() {
+    return(
+        <Header 
+        image={image} 
+        description="Ilustração de mulher com celular">
+            Meus Projetos
+        </Header>
+    )
+}
+
+export default Portfolio
