413911eb450e128e8d326adab8eb23ad7feacda3
Mariana Caetano Pereira <nanah.caetano@gmail.com>
2022-07-15 23:09:51 -0300

    added footer component in the routes

--- src/routes/ApplicationRoutes.jsx
+++ src/routes/ApplicationRoutes.jsx
@@ -3,17 +3,21 @@ import Sobre from '../pages/Sobre/Sobre';
 import Portfolio from '../pages/Portfolio/Portfolio'
 import Comentarios from '../pages/Comentarios/Comentarios'
 import Menu from '../components/Menu/Menu'
+import Footer from '../components/Footer/Footer';
 
 function ApplicationRoutes() {
     return(
+        <>
         <BrowserRouter>
             <Menu />
             <Routes>
-                <Route path="/" element={<Sobre />}/>
-                <Route path="/portfolio" element={<Portfolio />}/>
-                <Route path="/comentarios" element={<Comentarios />}/>
+                <Route path="/" element={<Sobre />} />
+                <Route path="/portfolio" element={<Portfolio />} />
+                <Route path="/comentarios" element={<Comentarios />} />
             </Routes>
         </BrowserRouter>
+        <Footer footerText="Feito com amor por Mariana" />
+        </>
     )
 }
 
